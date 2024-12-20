08c8980f4e1dd249754201f62cc31231bd2f3fc6
L_Emanuel <43679743+Luis-Emanuel@users.noreply.github.com>
2024-12-20 17:28:28 -0300

    Criando componentes
    Utilização da Lib styled components para desenvolver os componentes.
    Criação dos componentes "Banner, Button, Corousel, Footer".
    Dados dos vídeos e seções no arquivo "dados_iniciais.json".

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import Menu from "./components/Menu";
+import "./"
+import BannerMain from "./components/BannerMain";
+import dadosIniciais from "./data/dados_iniciais.json";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Menu/>
+      <BannerMain 
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end? Trabalhando na área O npx não é o npm, pois no npx ele instala o pacote temporariamente e depois de usarmos ele deleta. Stala o pacote temporariamente. Acote temporariamente e depois de"}
+      />
+
     </div>
   );
 }
