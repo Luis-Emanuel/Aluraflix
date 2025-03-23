d0496453afd9b898291b33ee50b3c4eb639ddfe2
L_Emanuel <43679743+Luis-Emanuel@users.noreply.github.com>
2025-03-23 15:48:10 -0300

    .

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -8,8 +8,9 @@ function Menu() {
       <a href='/'>
         <img className={"Logo"} src={Logo} alt='Logo do AluraFlix' />
       </a>
-
-      <ButtonLink className='ButtonLink' href='/'>Novo vídeo</ButtonLink>
+      <ButtonLink className='ButtonLink' href='/'>
+        Novo vídeo
+      </ButtonLink>
     </nav>
   )
 }
